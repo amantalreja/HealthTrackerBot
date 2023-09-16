1071f2b6345616d0ea9feb3f5c09fa1cf3193744
Aman Talreja <64047297+amantalreja@users.noreply.github.com>
2023-09-16 16:54:09 +0000

    1st page done

--- HealthTrackerBot/src/pages/App.js
+++ HealthTrackerBot/src/pages/App.js
@@ -1,5 +1,26 @@
+import "./App.css";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
-    return <h1>Home</h1>;
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate('/Blogs'); // Navigate to the '/about' route
   };
-  
-  export default Home;
+
+  return <div>
+    <div className="centerPortion">
+      <div className="heading">
+        <p className="talkTo">Talk To</p>
+        <p className="talkTo">Claude</p>
+      </div>
+      <div className="loginPortion">
+        <p className="email">Email Address</p>
+        <input placeholder="Email "></input>
+        <button onClick={handleButtonClick}>Continue with Email</button>
+      </div>
+    </div>
+  </div>;
+};
+
+export default Home;
--- HealthTrackerBot/src/pages/Blogs.js
+++ HealthTrackerBot/src/pages/Blogs.js
@@ -1,5 +1,7 @@
 const Blogs = () => {
-    return <h1>Blog Articles</h1>;
-  };
-  
-  export default Blogs;
+  return <div>
+
+  </div>;
+};
+
+export default Blogs;
